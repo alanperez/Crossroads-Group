cfedf7adb7405f64509215db01b6c40710fb9926
perezxalan <perezxalan@gmail.com>
2022-05-25 18:51:39 -0500

    utilized our interface/service in order to fetch our data

--- src/app/components/github-commit/github-commit.component.ts
+++ src/app/components/github-commit/github-commit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GithubService } from 'src/app/service/github.service';
-
+import {CommitObject} from 'src/app/interface/commit.interface';
 @Component({
   selector: 'app-github-commit',
   templateUrl: './github-commit.component.html',
@@ -8,13 +8,16 @@ import { GithubService } from 'src/app/service/github.service';
 })
 export class GithubCommitComponent implements OnInit {
 
+  // Assign our interface
+  commits: CommitObject[] = [];
   constructor(private githubService: GithubService) { }
 
   ngOnInit(): void {
     // 
     this.githubService.getCommits().subscribe(
-      (results: any) => {
-        console.log(results)
+      (results) => {
+        this.commits = results
+        console.log('clg', results)
       }
     )
 
