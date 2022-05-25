cb5f77d4ba81352dd34476de12336ee655aeac34
perezxalan <perezxalan@gmail.com>
2022-05-24 20:03:26 -0500

    fetching our commits, console logging the results to view whats returned

--- src/app/components/github-commit/github-commit.component.ts
+++ src/app/components/github-commit/github-commit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubService } from 'src/app/service/github.service';
 
 @Component({
   selector: 'app-github-commit',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubCommitComponent implements OnInit {
 
-  constructor() { }
+  constructor(private githubService: GithubService) { }
 
   ngOnInit(): void {
+    // 
+    this.githubService.getCommits().subscribe(
+      (results: any) => {
+        console.log(results)
+      }
+    )
+
   }
 
 }
