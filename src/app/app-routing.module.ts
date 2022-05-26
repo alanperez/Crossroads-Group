7b2c1ace15df4db78e7877928ebf5a6d4ba5166b
perezxalan <perezxalan@gmail.com>
2022-05-25 21:06:30 -0500

    fixed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GithubCommitComponent } from './components/github-commit/github-commit.component';
 const routes: Routes = [
-  {path: 'commits', component: GithubCommitComponent},
-  {path: '**', redirectTo: 'commits'}
+  {path: '', component: GithubCommitComponent},
+  {path: '**', component: GithubCommitComponent}
 ];
 
 @NgModule({
